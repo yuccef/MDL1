fa420bcabdb66b10c4f4a35b344419de9bd0c6d7
youssef ABBADI <youssefabbadipro@gmail.com>
2023-02-28 09:33:29 +0100

    Appeler les deux fonctions dans le main

--- test.js
+++ test.js
@@ -1,64 +1,73 @@
-const fs = require('fs'); 
-let user = fs.readFileSync('users.json'); //importer le fichier "users.json"
+const fs = require("fs"); 
+let user = fs.readFileSync("users.json"); //importer le fichier "users.json"
 const tab = JSON.parse(user);  //recupérer le tableau du fichier importé
 
 
-
-
-
 //Parcourir le tableau en enregistrant les pays/sociétés dans une map tout en incrementant la valeurs pour les doublant.
-
+ 
+/**
+ * 
+ */
 function Liste_Pays_Utilisateurs()
 {
-var Mapp = new Map;
+    var Mapp = new Map;
 
-//Parcourir le tableau en enregistrant les pays/sociétés dans une map tout en incrementant la valeurs pour les doublant.
-for(var i=0;i<tab.length; i++)
-{
-
-    if (Mapp.has(tab[i].country)) //cas ou la map contient déja ce pays comme clé
+    //Parcourir le tableau en enregistrant les pays/sociétés dans une map tout en incrementant la valeurs pour les doublant.
+    for(var i=0;i<tab.length; i++)
     {
-        Mapp.set(tab[i].country, Mapp.get(tab[i].country) + 1);   //incrementer la valeurs de ce clé
+
+        if (Mapp.has(tab[i].country)) //cas ou la map contient déja ce pays comme clé
+        {
+            Mapp.set(tab[i].country, Mapp.get(tab[i].country) + 1);   //incrementer la valeurs de ce clé
+        }
+  
+        else      
+        {
+            Mapp.set(tab[i].country,1);    // ajouter le nouveau pays comme clé en le donnant une valeurs de 1
+        }
+
     }
 
-    else      
-    {
-        Mapp.set(tab[i].country,1);    // ajouter le nouveau pays comme clé en le donnant une valeurs de 1
-    }
 
-}
-
-console.log(Mapp);
+    console.log(Mapp);
 }
 
 
 
 function Liste_Société_Utilisateurs()
 {
-var Mapp = new Map;
+    var Mapp = new Map;
 
-for(var i=0;i<tab.length; i++)
-{
-
-    if (Mapp.has(tab[i].company))
+    for(var i=0;i<tab.length; i++)
     {
-        Mapp.set(tab[i].company, Mapp.get(tab[i].company) + 1);
+
+        if (Mapp.has(tab[i].company))
+        {
+            Mapp.set(tab[i].company, Mapp.get(tab[i].company) + 1);
+        }
+
+        else
+        {
+            Mapp.set(tab[i].company,1);
+        }
+
     }
 
-    else
-    {
-        Mapp.set(tab[i].company,1);
-    }
-
-}
-
-console.log(Mapp);
+    console.log(Mapp);
 }
 
 //Pour afficher il suffit d'appler l'une des deux fonctions
 
 
+console.log((Liste_Société_Utilisateurs()));
+console.log((Liste_Pays_Utilisateurs()));
 
+
+
+
+
+
+/////// pour afficher le fichier dans un serveur ./////
 /*
 const http = require('http');
 
@@ -74,8 +83,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-
 */
 
