03e80e676b10102ce6b8e887e5e1e8b5a9107e2b
youssef ABBADI <youssefabbadipro@gmail.com>
2023-03-04 23:55:00 +0100

    Changing console color & editing code

--- test.js
+++ test.js
@@ -1,10 +1,26 @@
 const fs = require("fs"); 
 let user = fs.readFileSync("users.json"); //importer le fichier "users.json"
 const tab = JSON.parse(user);  //recupérer le tableau du fichier importé
+const prompt = require("prompt-sync")();    //inclusion  de prompt
 
 
-//Parcourir le tableau en enregistrant les pays/sociétés dans une map tout en incrementant la valeurs pour les doublant.
- 
+// la liste des couleurs  
+//     Rouge : \x1b[31m
+//     Vert : \x1b[32m
+//     Jaune : \x1b[33m
+//     Bleu : \x1b[34m
+//     Cyan : \x1b[36m
+//     Blanc : \x1b[37m
+
+
+//demander la couleur voulu dans le console
+console.log("Vous choisissez quelle couleur? \n\
+1: Cyan  \n\
+2: rouge       \n\
+3: jaune :\n ");
+
+const v = prompt(" ");  //récupérer la valeur taper par l'utilisateur
+
 
 
 /**
@@ -29,10 +45,21 @@ function Liste_Pays_Utilisateurs()
         }
 
     }
+    //partie d'affichage
+    if(v==1){
+        console.log("\x1b[36m%s\x1b[0m",Mapp); //le cyan
+    }
+    else if (v==2) {
+        console.log("\x1b[31m%s\x1b[0m",Mapp);//le rouge 
+    }
+    else if (v==3) {
+        console.log("\x1b[33m%s\x1b[0m",Mapp); //le jaune
+    }
 
-
-    console.log(Mapp);
 }
+
+
+
 
 
 /**
@@ -41,7 +68,7 @@ function Liste_Pays_Utilisateurs()
 function Liste_Société_Utilisateurs()
 {
     var Mapp = new Map;
-
+  
     for(var i=0;i<tab.length; i++)
     {
 
@@ -56,18 +83,22 @@ function Liste_Société_Utilisateurs()
         }
 
     }
+    if(v==1){
+        console.log("\x1b[36m%s\x1b[0m",Mapp); //le cyan
+    }
+    else if (v==2) {
+        console.log("\x1b[31m%s\x1b[0m",Mapp);//le rouge 
+    }
+    else if (v==3) {
+        console.log("\x1b[33m%s\x1b[0m",Mapp); //le jaune
+    }
 
-    console.log(Mapp);
 }
-
-
-//Pour afficher il suffit d'appler l'une des deux fonctions (console)
-
-const prompt = require("prompt-sync")();    //inclusion  de prompt
+   
 
 
 //on a utiliser le console.log puisque le \n ne marche pas avec le prompt
-console.log("   C'est quoi votre choix ? \n\
+console.log("C'est quoi votre choix ? \n\
 1: Société   \n\
 2: Pays       \n\
 3: Quitter :\n ");
@@ -75,17 +106,33 @@ console.log("   C'est quoi votre choix ? \n\
 
 const c = prompt(" ");  //récupérer la valeur taper par l'utilisateur
 
-console.log(`votre choix est :  ${c}`);
+console.log(`vous avez choisi la couleur numero :  ${v}`); //le numero de couleur
+console.log(`et votre choix est du numero :  ${c}`); //pour afficher le choix saisi
 
 
-if(c==1){
-    console.log((Liste_Société_Utilisateurs())); //les societes
+
+
+
+if(c==1){ //les societes
+   
+    console.log((Liste_Société_Utilisateurs())); 
+  
 }
-if(c==2){
-    console.log((Liste_Pays_Utilisateurs()));  //les pays
+if(c==2){  //les pays   
+    console.log((Liste_Pays_Utilisateurs())); 
 }
-if(c==3){
-    console.log("Good bye");   //exit 
+   
+
+if(c==3){     //exit  
+    if(v==1){
+        console.log("\x1b[36m%s\x1b[0m","Good bye"); 
+    }
+    else if (v==2) {
+        console.log("\x1b[31m%s\x1b[0m","Good bye");
+    }
+    else if (v==3) {
+        console.log("\x1b[33m%s\x1b[0m","Good bye");
+    }
 }
 
 
